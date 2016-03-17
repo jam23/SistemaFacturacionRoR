ba875f12f6668497be2b5ffb0bea60152dd31dd6
jam23 <juan.acosta.morillo@gmail.com>
2016-03-17 07:26:20 -0430

    Layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( AdminLTE.min.css )
+Rails.application.config.assets.precompile += %w( skins/skin-blue.min )
+
+Rails.application.config.assets.precompile += %w( plugins/jQuery/jQuery-2.1.4.min )
+Rails.application.config.assets.precompile += %w( bootstrap.min )
+Rails.application.config.assets.precompile += %w( app.min )
+Rails.application.config.assets.precompile += %w( plugins/dataTables/jquery.dataTables.min.css )
+Rails.application.config.assets.precompile += %w( plugins/dataTables/jquery.dataTables.min.js )
